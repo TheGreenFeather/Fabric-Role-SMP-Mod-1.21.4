f21168782773ff630a3cf1f7bf6813c241ef5396
TheGreenFeather <nguyenvuong17102008@gmail.com>
2024-12-12 21:12:05 +0700

    Fix Item ID not set

--- src/main/java/io/github/thegreenfeather/rolesmp/item/ModItems.java
+++ src/main/java/io/github/thegreenfeather/rolesmp/item/ModItems.java
@@ -6,6 +6,8 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
 import net.minecraft.util.Identifier;
 
 public class ModItems {
@@ -13,7 +15,7 @@ public class ModItems {
     public static final Item DYNAMITE = registerItem("dynamite", new Item(new Item.Settings()));
 
     private static Item registerItem(String name, Item item) {
-        return Registry.register(Registries.ITEM, Identifier.of(RoleSMPMod.MOD_ID, name), item);
+        return Registry.register(Registries.ITEM, RegistryKey.of(RegistryKeys.ITEM, Identifier.of(RoleSMPMod.MOD_ID, name)), item);
     }
 
     public static void registerModItems() {
